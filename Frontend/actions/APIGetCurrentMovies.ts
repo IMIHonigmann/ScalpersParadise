88bca749bb6de2ed325e1b332b7ca131e6cc5f89
Homam Mousa <homammousa15@gmail.com>
2025-06-29 15:12:13 +0200

    changed fetching logic for the home page

--- Frontend/actions/APIGetCurrentMovies.ts
+++ Frontend/actions/APIGetCurrentMovies.ts
@@ -1,5 +1,8 @@
 'use server';
 
+import { type TMDBMovieDetails } from '@/types/TMDB';
+import { getMovieById } from './TMDBGetMovieById';
+
 export async function getCurrentMoviesByName(movieName?: string) {
   const JWT = ``;
   const res = await fetch(
@@ -19,5 +22,14 @@ export async function getCurrentMoviesByName(movieName?: string) {
     throw new Error(`Failed to fetch: Error Code: ${res.status}`);
   }
 
-  return res.json();
+  const currentMovies = await res.json();
+
+  const movieDetailsResults = await Promise.allSettled(
+    currentMovies.map(async movie => await getMovieById(movie.movieId))
+  );
+  const TMDBMovieDetails: TMDBMovieDetails[] = movieDetailsResults
+    .filter(result => result.status === 'fulfilled')
+    .map(result => (result as PromiseFulfilledResult<TMDBMovieDetails>).value);
+
+  return TMDBMovieDetails;
 }
--- Frontend/app/home/page.tsx
+++ Frontend/app/home/page.tsx
@@ -1,16 +1,26 @@
 import { getCurrentMoviesByName } from '@/actions/APIGetCurrentMovies';
-import React from 'react';
+import Image from 'next/image';
 
 type Props = {};
 
 export default async function Home({}: Props) {
   const currentMovies = await getCurrentMoviesByName();
-  console.log(currentMovies);
+  console.log('bruh', currentMovies);
   return (
-    <>
-      {currentMovies.map((movie, index: number) => (
-        <div key={index}>{movie.movieName ? movie.movieName : 'No Name'}</div>
-      ))}
-    </>
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="flex gap-4">
+        {currentMovies.map((movie, index: number) => (
+          <div className="text-center" key={index}>
+            <Image
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={''}
+              width={500}
+              height={500}
+            />
+            <div>{movie.title}</div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
