d9b0895bae418c28ca31cace494ac5a4af14ebc2
Homam Mousa <homammousa15@gmail.com>
2025-07-09 20:18:59 +0200

    added redirections to 3D Elements and changed canvas positioning

--- Frontend/app/home/MovieBox.tsx
+++ Frontend/app/home/MovieBox.tsx
@@ -1,21 +1,31 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import * as THREE from 'three';
 import React, { useRef, useState } from 'react';
 import { Canvas, useFrame, ThreeElements } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 
-function Box(props: ThreeElements['mesh']) {
+function Box(
+  props: ThreeElements['mesh'] & {
+    setCamLoc: React.Dispatch<React.SetStateAction<number>>;
+  }
+) {
   const meshRef = useRef<THREE.Mesh>(null!);
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
   useFrame((state, delta) => (meshRef.current.rotation.x += delta * 0));
+  const router = useRouter();
   return (
     <mesh
       {...props}
       ref={meshRef}
       scale={active ? 1.5 : 1}
-      onClick={() => setActive(!active)}
+      onClick={() => {
+        setActive(!active);
+        props.setCamLoc((prev: number) => prev + 0.1);
+        router.push('/test');
+      }}
       onPointerOver={() => setHover(true)}
       onPointerOut={() => setHover(false)}
     >
@@ -26,9 +36,13 @@ function Box(props: ThreeElements['mesh']) {
 }
 
 export function BoxCanvas() {
+  const [camLoc, setCamLoc] = useState(2.5);
   return (
-    <div style={{ width: '600px', height: '300px' }}>
-      <Canvas camera={{ position: [0, 0, -2], fov: 75 }}>
+    <div className="flex justify-center items-center">
+      <Canvas
+        camera={{ position: [0, 0, -2], fov: 75 }}
+        style={{ width: '100%', height: '300px' }}
+      >
         <OrbitControls
           minPolarAngle={Math.PI / 4}
           maxPolarAngle={Math.PI / 2}
@@ -37,16 +51,12 @@ export function BoxCanvas() {
           enablePan={false}
           enableZoom={false}
         />
-        <ambientLight intensity={Math.PI / 2} />
-        <spotLight
-          position={[10, 10, 10]}
-          angle={0.15}
-          penumbra={1}
-          decay={0}
-          intensity={Math.PI}
-        />
-        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-        <Box position={[0, 0, 0]} />
+        <pointLight position={[10, 10, 10]} decay={0} intensity={Math.PI * 4} />
+        <Box position={[camLoc * -2, 0, 0]} setCamLoc={setCamLoc} />
+        <Box position={[camLoc * -1, 0, 0]} setCamLoc={setCamLoc} />
+        <Box position={[camLoc * 0, 0, 0]} setCamLoc={setCamLoc} />
+        <Box position={[camLoc * 1, 0, 0]} setCamLoc={setCamLoc} />
+        <Box position={[camLoc * 2, 0, 0]} setCamLoc={setCamLoc} />
       </Canvas>
     </div>
   );
