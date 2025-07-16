5aca77375345689deb9a52791b9a7e619bef1039
Homam Mousa <homammousa15@gmail.com>
2025-07-16 13:09:59 +0200

    added hamburger menu navbar for mobile devices

--- Frontend/components/Header.tsx
+++ Frontend/components/Header.tsx
@@ -1,9 +1,9 @@
 import { PiTextAlignLeft } from 'react-icons/pi';
 import { HomeLogo } from './HomeLogo';
-import Link from 'next/link';
 import { CiSearch } from 'react-icons/ci';
 import { Oswald } from 'next/font/google';
 import dynamic from 'next/dynamic';
+import Navbar from './Navbar';
 
 const MovieSearcher = dynamic(() => import('@/components/MovieSearcher'));
 
@@ -25,26 +25,18 @@ export default function Header() {
 
   return (
     <div
-      className={`flex justify-between relative text-right items-start w-full p-8 tracking-widest`}
+      className={`flex justify-center gap-x-2 relative mx-auto text-right items-start w-full p-8 tracking-widest`}
     >
       <HomeLogo />
-      <nav className="w-[10%]">
-        <ul className="flex flex-col space-y-0 text-xl">
-          {navItems.map((item, index) => (
-            <li
-              key={index}
-              className="hover:opacity-70 transition-opacity cursor-pointer"
-            >
-              <Link href={item.path}>{item.label}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <div className="flex-[2]" />
+      <Navbar
+        navItems={navItems}
+        className="w-1/4 lg:w-[10%] order-last md:order-1"
+      />
+      <div className="flex-[2] order-1" />
       <span
         className="w-3/4
             flex justify-between items-center relative
-          after:content-[''] after:absolute after:-bottom-3 after:left-1/2 after:-translate-x-1/2 after:w-[98%] after:h-px after:bg-current"
+          after:content-[''] after:absolute after:-bottom-3 after:left-1/2 after:-translate-x-1/2 after:w-[98%] after:h-px after:bg-current order-2"
       >
         <span
           className={`flex items-center w-full text-left normal-case ${oswald.className} tracking-normal`}
---
+++ Frontend/components/Navbar.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import Link from 'next/link';
+import { useState } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+
+type props = {
+  navItems: Array<{ label: string; path: string }>;
+  className?: string;
+};
+
+export default function Navbar({ navItems, className }: props) {
+  const [isOpen, setIsOpen] = useState(true);
+  return (
+    <nav className={` ${className ?? ''}`}>
+      <GiHamburgerMenu
+        onClick={() => setIsOpen(prev => !prev)}
+        className={`text-6xl md:hidden transition-transform duration-300 ml-auto ${
+          isOpen ? 'rotate-90' : 'rotate-0'
+        }`}
+      />
+      <ul className={`flex flex-col space-y-0 text-xl`}>
+        {navItems.map((item, index) => (
+          <li
+            key={index}
+            className={`hover:opacity-70 cursor-pointer transition-all duration-300 ${
+              isOpen ? 'opacity-100 visible' : 'opacity-0 invisible'
+            }`}
+            style={{
+              transform: `translateX(${isOpen ? '0' : '100%'})`,
+              transitionDelay: `${0.075 * index}s`,
+            }}
+          >
+            <Link href={item.path}>{item.label}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
