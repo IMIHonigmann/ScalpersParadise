0a0039465d241c615f358965375ebc091d009143
Homam Mousa <homammousa15@gmail.com>
2025-07-17 12:07:32 +0200

    tweaked the sizes

--- Frontend/app/[movieId]/page.tsx
+++ Frontend/app/[movieId]/page.tsx
@@ -137,13 +137,13 @@ export async function MoviePreview({
                       className={`text-yellow-400 font-bold min-w-3/5 max-w-full pr-4 
                         ${
                           movie.title.length <= 15
-                            ? 'text-9xl'
+                            ? 'text-8xl'
                             : movie.title.length <= 25
-                            ? 'text-8xl lg:text-9xl'
+                            ? 'text-7xl lg:text-9xl'
                             : movie.title.length <= 40
-                            ? 'text-6xl lg:text-7xl 2xl:text-8xl'
+                            ? 'text-5xl lg:text-7xl 2xl:text-8xl'
                             : movie.title.length <= 60
-                            ? 'text-5xl lg:text-6xl 2xl:text-7xl'
+                            ? 'text-4xl lg:text-6xl 2xl:text-7xl'
                             : 'text-xl md:text-2xl lg:text-4xl 2xl:text-5xl'
                         }`}
                     >
