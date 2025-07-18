5f09b62b68333461fd9fd5d15c469fc01e58ba98
Homam Mousa <homammousa15@gmail.com>
2025-07-18 14:41:38 +0200

    changed the layout from the slider to a grid with improved styling

--- Frontend/app/home/HeroSlider.tsx
+++ Frontend/app/home/HeroSlider.tsx
@@ -13,7 +13,7 @@ export default function HeroSlider({ highlightedMovies }: Props) {
   const [highlightedIndex, setHighlightedIndex] = useState(0);
   return (
     <div className="flex justify-center">
-      <div className="flex justify-center items-center w-1/2 text-7xl">
+      <div className="grid grid-cols-[50px_0.7fr_0.3fr_50px] items-center w-1/2 text-3xl">
         <MdNavigateBefore
           onClick={() =>
             setHighlightedIndex(
@@ -21,20 +21,24 @@ export default function HeroSlider({ highlightedMovies }: Props) {
                 (prev - 1 + highlightedMovies.length) % highlightedMovies.length
             )
           }
-          className="cursor-pointer"
+          className="cursor-pointer flex-shrink-0 text-7xl"
         />
         <Image
           src={`https://image.tmdb.org/t/p/w1280${highlightedMovies[highlightedIndex].backdrop_path}`}
           width={1280}
           height={720}
           alt={''}
-          className="cursor-pointer select-none w-5/6"
+          className="cursor-pointer select-none w-full"
         />
+        <div className="grid grid-rows-[100px_50px] gap-4 place-self-start">
+          <span>{highlightedMovies[highlightedIndex].title}</span>
+          <span>BRRRRRRRRR</span>
+        </div>
         <MdNavigateNext
           onClick={() =>
             setHighlightedIndex(prev => (prev + 1) % highlightedMovies.length)
           }
-          className="cursor-pointer"
+          className="cursor-pointer flex-shrink-0 text-7xl"
         />
       </div>
     </div>
