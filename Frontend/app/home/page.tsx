1bb0d1a11b298e0f259513fe97e77f222e458274
Homam Mousa <homammousa15@gmail.com>
2025-07-23 23:14:41 +0200

    styled movie hover on /home

--- Frontend/app/home/page.tsx
+++ Frontend/app/home/page.tsx
@@ -4,10 +4,9 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { BoxCanvas } from './MovieBox';
 import HeroSlider from './HeroSlider';
+import { MdOutlinePlayCircle } from 'react-icons/md';
 
-type Props = {};
-
-export default async function Home({}: Props) {
+export default async function Home() {
   const currentMovies = await getCurrentMovies();
   const highlightedMovies = [
     currentMovies[0],
@@ -22,14 +21,21 @@ export default async function Home({}: Props) {
       <div className="grid grid-cols-7 gap-12 min-h-screen p-32">
         {currentMovies.map((movie, index: number) => (
           <Link href={`/${movie.id}`} key={index} className="text-center group">
-            <Image
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              alt={''}
-              width={500}
-              height={500}
-              priority={index === 0}
-              className="rounded-md transition-transform transform-gpu duration-100 group-hover:scale-x-0"
-            />
+            <div className="relative overflow-hidden w-[200px] h-[300px] mx-auto flex items-center justify-center">
+              <Image
+                src={
+                  movie.poster_path
+                    ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                    : '/Sotenboori3DFallback.png'
+                }
+                alt={''}
+                width={500}
+                height={500}
+                priority={index === 0}
+                className="rounded-md transition-all transform-gpu duration-300 group-hover:scale-110 group-hover:blur-sm group-hover:rotate-1"
+              />
+              <MdOutlinePlayCircle className="absolute transition-all duration-500 inset-0 m-auto text-8xl opacity-0 group-hover:opacity-100 -translate-x-1 translate-y-3 rotate-[360deg] scale-150 group-hover:translate-y-0 group-hover:translate-x-0 group-hover:-rotate-12 group-hover:scale-100 origin-center pointer-events-none" />
+            </div>
             <div>{movie.title}</div>
           </Link>
         ))}
